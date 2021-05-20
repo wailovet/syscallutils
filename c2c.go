3371c03bb53da772ff96c38840bd5eab2f04ef3a
wailovet <wailovet@163.com>
2021-05-21 03:07:17 +0800

    fix

--- c2c.go
+++ c2c.go
@@ -2,8 +2,6 @@ package syscallutils
 
 import "unsafe"
 
-func PtrPtr(i uintptr, cb func(pp uintptr)) uintptr {
-	pp := uintptr(unsafe.Pointer(&i))
-	cb(pp)
-	return (uintptr)(unsafe.Pointer((*uintptr)(unsafe.Pointer(pp))))
+func PtrPtr(ret *uintptr) (b uintptr) {
+	return uintptr(unsafe.Pointer(ret))
 }
