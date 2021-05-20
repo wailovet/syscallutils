7496383edb6f508e8ddd37374510ccd7c91d6167
u <u@a.com>
2021-05-20 17:24:53 +0800

    init

---
+++ c2go.go
@@ -0,0 +1,59 @@
+package syscallutils
+
+import "unsafe"
+
+var C2Go C2GoBase
+
+type C2GoBase struct {
+}
+
+func (c2g *C2GoBase) Int(p uintptr) int {
+	return int(p)
+}
+
+func (c2g *C2GoBase) Int32(p uintptr) int32 {
+	return int32(p)
+}
+
+func (c2g *C2GoBase) Int64(p uintptr) int64 {
+	return int64(p)
+}
+
+func (c2g *C2GoBase) Float64Slice(p uintptr, len int) (ret []float64) {
+	for i := 0; i < len; i++ {
+		ret = append(ret, *((*float64)(unsafe.Pointer(p))))
+		p += unsafe.Sizeof(float64(0))
+	}
+	return ret
+}
+
+func (c2g *C2GoBase) Float32Slice(p uintptr, len int) (ret []float32) {
+	for i := 0; i < len; i++ {
+		ret = append(ret, *((*float32)(unsafe.Pointer(p))))
+		p += unsafe.Sizeof(float64(0))
+	}
+	return ret
+}
+
+func (c2g *C2GoBase) Bytes(p uintptr) (d []byte) {
+	ret := (*byte)(unsafe.Pointer(p))
+	// 遍历C返回的char指针，直到 '\0' 为止
+	for *ret != 0 {
+		d = append(d, *ret)
+		p += unsafe.Sizeof(byte(0))      // 移动指针，指向下一个char
+		ret = (*byte)(unsafe.Pointer(p)) // 获取指针的值，此时指针已经指向下一个char
+	}
+	return
+}
+
+func (c2g *C2GoBase) Bytes4Void(p uintptr, len int) (d []byte) {
+	for i := 0; i < len; i++ {
+		d = append(d, *(*byte)(unsafe.Pointer(p)))
+		p += unsafe.Sizeof(byte(0)) // 移动指针，指向下一个char
+	}
+	return
+}
+
+func (c2g *C2GoBase) String4CharPtr(p uintptr) string {
+	return string(c2g.Bytes(p))
+}
---
+++ go2c.go
@@ -0,0 +1,41 @@
+package syscallutils
+
+import (
+	"unsafe"
+)
+
+var Go2C Go2CBase
+
+type Go2CBase struct {
+}
+
+func (g2c *Go2CBase) AnyPtr(d interface{}) uintptr {
+	return uintptr(unsafe.Pointer(&d))
+}
+
+func (g2c *Go2CBase) Float64Slice(d []float64) uintptr {
+	ret := unsafe.Pointer(&d[0])
+	return uintptr(ret)
+}
+
+func (g2c *Go2CBase) Float32Slice(d []float64) uintptr {
+	ret := unsafe.Pointer(&d[0])
+	return uintptr(ret)
+}
+
+func (g2c *Go2CBase) Int(d int) uintptr {
+	return uintptr(d)
+}
+
+func (g2c *Go2CBase) Int32(d int) uintptr {
+	return uintptr(d)
+}
+
+func (g2c *Go2CBase) Int64(d int) uintptr {
+	return uintptr(d)
+}
+
+func (g2c *Go2CBase) Chars(d string) uintptr {
+	b := append([]byte(d), 0)
+	return uintptr(unsafe.Pointer(&b[0]))
+}
