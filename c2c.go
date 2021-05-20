cdc9113e027fc62ac162b35316051c8beafcb5c0
wailovet <wailovet@163.com>
2021-05-21 02:54:07 +0800

    fix

--- c2c.go
+++ c2c.go
@@ -5,5 +5,5 @@ import "unsafe"
 func PtrPtr(i uintptr, cb func(pp uintptr)) uintptr {
 	pp := uintptr(unsafe.Pointer(&i))
 	cb(pp)
-	return (uintptr)(unsafe.Pointer((*uintptr)(unsafe.Pointer(ptr))))
+	return (uintptr)(unsafe.Pointer((*uintptr)(unsafe.Pointer(pp))))
 }
